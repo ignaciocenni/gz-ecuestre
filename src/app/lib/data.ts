7ec0b05d978a9cb117ca2c4b8e02f1156650c50c
tod1az <t.diaz.soto@gmail.com>
2023-12-26 20:07:15 -0300

    add(app/lib data.ts) : add data file with mock data

---
+++ src/app/lib/data.ts
@@ -0,0 +1,53 @@
+export const horses = [
+  {
+    id: 'rp5',
+    name: 'Relámpago',
+    breed: 'Pura Sangre',
+    age: 5
+  },
+  {
+    id: 'ea7',
+    name: 'Esmeralda',
+    breed: 'Árabe',
+    age: 7
+  },
+  {
+    id: 'ca4',
+    name: 'Centella',
+    breed: 'Appaloosa',
+    age: 4
+  },
+  {
+    id: 'rt6',
+    name: 'Rayo',
+    breed: 'Trotón Francés',
+    age: 6
+  }
+]
+
+export const breeds = [
+  {
+    id: 1,
+    name: 'Pura Sangre'
+  },
+  {
+    id: 2,
+    name: 'Árabe'
+  },
+  {
+    id: 3,
+    name: 'Appaloosa'
+  },
+  {
+    id: 4,
+    name: 'Trotón Francés'
+  },
+  {
+    id: 5,
+    name: 'Clydesdale'
+  },
+  {
+    id: 6,
+    name: 'Shire'
+  }
+]
