78131c217a5b6c8a48fcce239fb1b744bc5a4ba6
tod1az <t.diaz.soto@gmail.com>
2023-12-26 20:11:44 -0300

    add(/app page.tsx): add layout mockup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
-import Image from "next/image";
+import CardContainer from "@/components/CardContainer"
+import { horses, breeds } from "./lib/data"
+import DropDown from "@/components/DropDown"
+import SearchBar from "@/components/SearchBar"
 
 export default function Home() {
-  return <></>;
+  return (
+    <main className="flex justify-center gap-2 h-screen">
+      <aside className="hidden py-5 lg:flex flex-col items-center  border w-[30rem]">
+        <SearchBar />
+        <DropDown options={breeds} name="razas" />
+        <DropDown options={breeds} name="color" />
+        <DropDown options={breeds} name="edad" />
+      </aside>
+      <CardContainer horses={horses} />
+      <aside className="hidden lg:block border w-[15rem]">
+        ads
+      </aside>
+    </main>
+  )
 }
