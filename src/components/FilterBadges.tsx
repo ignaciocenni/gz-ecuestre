9fedcb9df0ffb51c30eb4a685957d502ec32426a
tod1az <t.diaz.soto@gmail.com>
2023-12-27 20:21:03 -0300

    feat(app/components FilterBadges.tsx): add FilterBadge Component

---
+++ src/components/FilterBadges.tsx
@@ -0,0 +1,11 @@
+import { IoIosCloseCircle } from "react-icons/io";
+
+export default function FilterBadge({ children }: { children: React.ReactNode }) {
+  return (
+    <div
+      className=" text-lg flex items-center justify-center gap-2 px-2 py-1 bg-blanco/50 border-2 border-negro rounded-full capitalize">
+      {children}
+      <button className="text-2xl"><IoIosCloseCircle /></button>
+    </div>
+  )
+}
