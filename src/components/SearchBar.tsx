5db0c08bce78cd3b59ce510c45c7610abfea3425
tod1az <t.diaz.soto@gmail.com>
2023-12-27 20:24:50 -0300

    refactor(/components SearchBar.tsx): fix fontsize

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,13 +2,13 @@ import { CiSearch } from "react-icons/ci"
 
 export default function SearchBar() {
   return (
-    <form className="flex items-center gap-1">
+    <form className="flex items-center gap-1 text-xl">
       <input
         type="text"
         placeholder="Buscar"
         className=" w-full max-w-xs px-2 py-1 rounded-lg outline"
       />
-      <button className="rounded-lg bg-gris px-2 py-1 h-[3rem] outline">
+      <button className="rounded-lg bg-gris px-2 py-1 h-[2.3rem] outline">
         <CiSearch />
       </button>
     </form>
