c5a66a139adcf365202d0e4596a03b860da46a20
tod1az <t.diaz.soto@gmail.com>
2023-12-27 20:23:03 -0300

    refactor(/components SearchBar.tsx): refactor search bar styles

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,8 +3,12 @@ import { CiSearch } from "react-icons/ci"
 export default function SearchBar() {
   return (
     <form className="flex items-center gap-1">
-      <input type="text" placeholder="Buscar" className="relative input input-bordered w-full max-w-xs" />
-      <button className="btn btn-neutral">
+      <input
+        type="text"
+        placeholder="Buscar"
+        className=" w-full max-w-xs px-2 py-1 rounded-lg outline"
+      />
+      <button className="rounded-lg bg-gris px-2 py-1 h-[3rem] outline">
         <CiSearch />
       </button>
     </form>
