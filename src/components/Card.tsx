f27115ee63a89a9ded801c33f7a2e4e0ed0e14cb
tod1az <t.diaz.soto@gmail.com>
2023-12-28 10:56:32 -0300

    refactor(/components card.tsx): card style changes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
 import { type Horse } from "@/app/lib/types"
 import Image from "next/image"
+import CardBadge from "./CardBadge"
 
 type CardProps = {
   horse: Horse
@@ -7,19 +8,23 @@ type CardProps = {
 
 export default function Card({ horse }: CardProps) {
   return (
-    <article className="flex flex-col  rounded-xl gap-2 text-center h-[20rem] w-[18rem] bg-base-100 shadow-xl overflow-hidden">
+    <article className="flex flex-col mt-[2rem]  text-gris rounded-xl gap-1 text-center h-[20rem] w-[14rem] bg-blanco shadow-xl overflow-hidden">
       <Image
-        className=" w-[18rem] h-[10rem]"
+        className="w-full h-[9rem]"
         src={'/caballo.jpg'}
         alt="caballo"
         width={400}
         height={400}
         quality={100}
       />
-      <h2 className="mt-4 text-sm font-thin">{horse.name}</h2>
-      <section className="flex gap-4 justify-center">
-        <h3 className="badge">{horse.breed}</h3>
-        <div className="badge">{horse.age}</div>
+      <div className="text-left px-7 mb-4">
+        <h2 className="mt-4 text-sm font-thin">{horse.name}</h2>
+        <h3 className="font-bold" >$9999</h3>
+      </div>
+      <section className="px-7 flex flex-wrap text-center text-nowrap  gap-2 justify-center">
+        <CardBadge>{horse.breed}</CardBadge>
+        <CardBadge>{`${horse.age} años`}</CardBadge>
+        <CardBadge>{horse.sex}</CardBadge>
       </section>
     </article>
   )
