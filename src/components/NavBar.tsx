d41bad88b7095081bc1f59f94fe3ba5ba19a3cc9
tod1az <t.diaz.soto@gmail.com>
2023-12-28 14:00:17 -0300

    fix(/components NavBar): add shadow to the navBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import Drawer from "./Drawer";
 export default function NavBar() {
   return (
     <>
-      <nav className="top-0 bg-blanco flex justify-around items-center fixed  h-[4.7rem] text-2xl border-red-900 border w-screen">
+      <nav className="top-0 bg-blanco flex justify-around items-center fixed  h-[4.7rem] text-2xl shadow-xl border w-screen">
         <Drawer />
         <Image src="/text-logo.png" alt="Logo" width={170} height={170} quality={100} className=" border-red-900 border" />
         {/*<h1 className="text-marron">GZ ECUESTRE</h1>*/}
