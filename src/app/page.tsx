3eebb71924870dce0f933671ae2b20b1c2fa6740
tod1az <t.diaz.soto@gmail.com>
2023-12-29 18:28:52 -0300

    feat(/components Footer.tsx): add footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import CardContainer from "@/components/CardContainer"
 import { horses } from "../lib/data"
+import Footer from "@/components/Footer"
 
 export default function Home() {
   return (
-    <main className="mt-[6rem] flex flex-col-reverse   xl:flex-row xl:ml-80  gap-5 ">
-      <CardContainer horses={horses} />
-      <aside className="h-[15rem] xl:h-screen xl:w-[16rem] border">
-        ads
-      </aside>
+    <main >
+      <section className="mt-[6rem] flex flex-col   xl:flex-row-reverse xl:ml-80  gap-5 ">
+        <aside className="h-[15rem] xl:h-screen xl:w-[16rem] border">
+          ads
+        </aside>
+        <CardContainer horses={horses} />
+      </section>
+      <Footer />
     </main>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,22 @@
+
+export default function Footer() {
+  return (
+    <footer className="flex flex-col items-center  w-full  bg-orange-200 h-[10rem] xl:h-[20rem]">
+      <ul className="grid grid-cols-4 px-2 gap-2 pt-2" >
+        <li className="hover:text-marron cursor-pointer">
+          <a className='underline' href="/a"> Sobre nosotros</a>
+        </li>
+        <li className="hover:text-marron cursor-pointer">
+          <a className='underline' href="/"> Sobre nosotros</a>
+        </li>
+        <li className="hover:text-marron cursor-pointer">
+          <a className='underline' href="/"> Sobre nosotros</a>
+        </li>
+        <li className="hover:text-marron cursor-pointer">
+          <a className='underline' href="/"> Sobre nosotros</a>
+        </li>
+      </ul>
+      <small>©GZ Ecuestre Todos los derechos reservados</small>
+    </footer>
+  )
+}
