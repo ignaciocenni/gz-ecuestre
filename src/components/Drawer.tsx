7f677f8e6c496d276df56164ef0384671e877e4b
tod1az <t.diaz.soto@gmail.com>
2023-12-29 16:51:03 -0300

    feat(/components Drawer.tsx): clicking outside the drawer will close it

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -10,7 +10,7 @@ export default function Drawer() {
 
   return (
     <section
-      className={clsx(`*:text-negro text-4xl fixed inset-0`
+      className={clsx(`text-negro text-4xl fixed inset-0`
         , { 'bg-black/30 backdrop-blur-sm': isOpen === true }
       )}>
       <input
@@ -18,14 +18,18 @@ export default function Drawer() {
         className="relative sr-only peer"
         type="checkbox"
         id="toggle"
+        checked={isOpen}
       />
       <label
         htmlFor="toggle"
-        className={`active:scale-110 peer-checked:-rotate-90  
+        className={`cursor-pointer active:scale-110 peer-checked:-rotate-90  
           transition-transform absolute top-3 delay-150 duration-300 xl:hidden left-1 `}
       >
         <FcMenu />
       </label>
+      {isOpen && <button
+        className="border -z-10 h-full w-full"
+        onClick={() => setIsOpen(!isOpen)} />}
       <div
         className={`mt-[4.56rem] top-0 absolute h-screen bg-orange-200 -left-[25rem] xl:left-0
         peer-checked:left-0 peer:transition ease-out delay-150 duration-300 shadow-xl  
