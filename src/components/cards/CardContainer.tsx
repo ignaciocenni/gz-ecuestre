a6f27fc45052ef877ff86b0b78f942a9247509c2
tod1az <t.diaz.soto@gmail.com>
2024-01-03 10:45:45 -0300

    change cards gap

--- src/components/cards/CardContainer.tsx
+++ src/components/cards/CardContainer.tsx
@@ -7,7 +7,7 @@ type CardContainerProps = {
 }
 export default function CardContainer({ horses }: CardContainerProps) {
   return (
-    <section className="flex flex-wrap mb-10 xl:w-10/12  justify-center gap-1">
+    <section className="flex flex-wrap mb-10 xl:w-10/12 xl:px-16 justify-center gap-1 md:gap-6">
       {
         horses.map((horse) => (
           <Fragment key={horse.id}>
